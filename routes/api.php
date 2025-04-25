ae8510da0cd85de19729153aa158f5729493a7d9
Ravi Poudel <ravi.poudel@nexdigitalstudio.com>
2025-04-25 06:32:30 +0545

    feat: add mechanics, customers & vehicles routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\CustomerController;
 use App\Http\Controllers\Api\MechanicController;
+use App\Http\Controllers\Api\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +10,9 @@ Route::get('/hello', function (Request $request) {
     return 'test hello';
 });
 
+Route::apiResource('mechanics', MechanicController::class);
+Route::apiResource('customers', CustomerController::class);
 
-Route::get('/mechanics', [MechanicController::class, 'index']);
+//[scoped] meaning -> the vehicle resource is always a part of customer i.e. vehicle is scoped by customer
+Route::apiResource('customers.vehicles', VehicleController::class)->scoped(['vehicle' => 'customer']);
+
